caac3d7fc9321ddd81dd04dc65fdb2a0432373c9
Soe Wei Yan Phyo <soeweiyan.phyo>
2023-07-30 19:33:43 +1000

    Add heading change effect

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const container = document.querySelector(".container");
 const signupBtn = document.querySelector(".signup-btn");
 const signinBtn = document.querySelector(".signin-btn");
+const headingSpan2 = document.querySelector(".heading-span-2");
 
-if (container && signupBtn && signinBtn) {
-  signupBtn.addEventListener("click", () => {
-    container.classList.add("change");
-  });
-  signinBtn.addEventListener("click", () => {
-    container.classList.remove("change");
-  });
-}
+signupBtn.addEventListener("click", () => {
+  container.classList.add("change");
+  setTimeout(() => {
+    headingSpan2.textContent = "Up";
+  }, 200);
+});
+
+signinBtn.addEventListener("click", () => {
+  container.classList.remove("change");
+  setTimeout(() => {
+    headingSpan2.textContent = "In";
+  }, 200);
+});
